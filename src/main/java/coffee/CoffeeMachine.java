74c0d761ac7307928605937edf34062dcd2a4cad
aepifanova393-lgtm <a.epifanova393@gmail.com>
2026-06-30 17:46:34 +0300

    Добавлены методы в класс CoffeeMachine Добавлены вызовы методов в классе App и обработаны исключения

--- src/main/java/coffee/CoffeeMachine.java
+++ src/main/java/coffee/CoffeeMachine.java
@@ -1,4 +1,21 @@
 package coffee;
 
-public class CoffeeMachine {
+public class CoffeeMachine  {
+
+    public void makeCoffee (int waterMl) throws NotEnoughWaterException {
+        if(waterMl >= 200) {
+            System.out.println("The coffee is ready.");
+        } else {
+            throw new NotEnoughWaterException("There is not enough water.");
+        }
+    }
+
+    public int calculateCups (int waterMl, int cupCapacityMl) {
+        int quantityOfCups = waterMl/cupCapacityMl;
+        return quantityOfCups;
+    }
+
+    public void printCoffeeName(String coffeeName) {
+        System.out.println(coffeeName.toUpperCase());
+    }
 }
